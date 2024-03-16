bbd1cd561dd757fee5e4bc6d6d3765a965630092
TamNhien <canhthien91@gmail.com>
2024-03-16 09:32:16 +0700

    Add project files.

---
+++ LTCB_TinhX^Y.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double x;
+    int y;
+
+    cout << "Nhap x (so thuc): ";
+    cin >> x;
+
+    cout << "Nhap y (so tu nhien): ";
+    cin >> y;
+
+    double result = pow(x, y);
+    cout << "Ket qua : " << result << endl;
+
+    return 0;
+}
